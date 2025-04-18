b58a9ba58be0bd1f1679469648ab4997f1f32bfd
XJ-Ong <oxj0887@gmail.com>
2025-04-18 11:32:52 +0800

    dddd

---
+++ src/main/java/com/xj/groupprojectapoms/GroupProjectAPOMS.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.xj.groupprojectapoms;
+
+/**
+ *
+ * @author prawn
+ */
+public class GroupProjectAPOMS {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
