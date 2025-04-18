0ccb2ecd534b2e76dbbf5ca9f2fd85549c84d81a
XJ-Ong <oxj0887@gmail.com>
2025-04-18 13:26:23 +0800

    added new code calculation

--- src/main/java/com/xj/groupprojectapoms/GroupProjectAPOMS.java
+++ src/main/java/com/xj/groupprojectapoms/GroupProjectAPOMS.java
@@ -12,5 +12,8 @@ public class GroupProjectAPOMS {
 
     public static void main(String[] args) {
         System.out.println("Test commit 2");
+        
+        int result = 1 + 2;
+        System.out.println(result);
     }
 }
