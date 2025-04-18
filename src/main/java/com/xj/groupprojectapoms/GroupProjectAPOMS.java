f32f511fcd17c6ac09938536ab93fa833e52941c
XJ-Ong <oxj0887@gmail.com>
2025-04-18 11:52:06 +0800

    test commit

--- src/main/java/com/xj/groupprojectapoms/GroupProjectAPOMS.java
+++ src/main/java/com/xj/groupprojectapoms/GroupProjectAPOMS.java
@@ -11,6 +11,6 @@ package com.xj.groupprojectapoms;
 public class GroupProjectAPOMS {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Test commit");
     }
 }
